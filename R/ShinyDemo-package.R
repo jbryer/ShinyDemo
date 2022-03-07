857d0bf565959bb0ea68f85cc953968ff3195112
Jason Bryer <jason@bryer.org>
2022-03-07 07:37:20 -0500

    Fixed DESCRIPTION file.

--- R/ShinyDemo-package.R
+++ R/ShinyDemo-package.R
@@ -1,8 +1,15 @@
-#' Shiny Package Demos
+#' Shiny Applications as Package Demos
+#' 
+#' This package is designed to run Shiny apps (Chang, et al,
+#' 2021 <https://shiny.rstudio.com>, Wickham, 2021 <isbn:9781492047384>)
+#' included in packages in the inst/shiny/ directory. In addition to
+#' providing a standardized way of running apps from within packages, it
+#' extends the typical process of running apps by allowing function
+#' parameters to be passed to the application. 
 #' 
 #' @name ShinyDemo-package
 #' @docType package
-#' @title Shiny Apps as Package Demos
+#' @title Shiny Applications as Package Demos
 #' @author \email{jason@@bryer.org}
 #' @keywords package shiny
 #' @import shiny
